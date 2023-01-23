1d2b958f0d5ee62cc224fd96f69af16aeab69ee0
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2023-01-23 12:53:00 +0100

    save

--- src/AppNavigation/AppNavigation.jsx
+++ src/AppNavigation/AppNavigation.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-import { useRoutes } from "react-router-dom";
-import LogIn from "../Auth/LogIn";
-import AppIndex from "./AppIndex";
-
- 
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import LogIn from '../Auth/LogIn'
+import AppIndex from './AppIndex'
+import Messages from './Messages'
 
 function AppNavigation() {
   let element = useRoutes([
     {
-      path: "/",
+      path: '/',
       element: <LogIn />,
       children: [{ index: true }],
     },
@@ -18,93 +17,12 @@ function AppNavigation() {
         { index: true, element: <LogIn /> },
 
         {
-          path: "/messages",
-          element: <Overview />,
+          path: '/messages',
+          element: <Messages />,
         },
-        {
-          path: "/registered-drugs",
-          element: <ViewRegisteredDrugs />,
-        },
-        {
-          path: "/register-new-drug",
-          element: <RegisterNewDrug />,
-        },
-        {
-          path: "/notifications",
-          element: <Notifications />,
-        },
-        {
-          path: "/account",
-          element: <Account />,
-        },
-        {
-          path: "/profile",
-          element: <Profile />,
-        },
-        {
-          path: "/QRCode",
-          element: <QRCodePage />,
-        },
-        {
-          path: "/GetHelp",
-          element: <Gethelp />,
-        },
-        {
-          path: "/sole-agents",
-          element: <SoleAgent />,
-        },
-        {
-          path: "/create-sole-agent",
-          element: <CreateMarketer/>,
-        },
-        {
-          path: "/marketers",
-          element: <Marketers />,
-        },
-        {
-          path: "/create-marketer",
-          element: <CreateMarketer />,
-        }
       ],
     },
-    {
-      path: "/KYC",
-      element: <KYC />,
-    },
-    {
-      path: "/KYCApproval",
-      element: <KYCApproval />,
-    },
-    {
-      path: "/view-KYC-approval",
-      element: <ViewKYCApproval />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/account/passphrass",
-      element: <Passphrase />,
-    },
-    
-    {
-      path: '/recorver-passphrase',
-      element: <RecPassphrase />
-    },
-    {
-      path: '/claim-token',
-      element: <ClaimToken />
-    },
-    {
-      path: '*',
-      element: <Page404 />
-    }
-  ]);
-  return element;
+  ])
+  return element
 }
-export default AppNavigation;
+export default AppNavigation
