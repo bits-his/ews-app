274e6526e285c605450680538f4b0817f6f843d9
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2023-01-23 12:40:15 +0100

    save

---
+++ src/AppNavigation/AppIndex.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Col, Row } from 'reactstrap'
+
+import Navbar from '../Menus/Navbar'
+import Sidebar from '../Menus/Sidebar'
+export default function AppIndex() {
+    return (
+        <div>
+            <Row className='m-0'>
+                <Col md={2} className='p-0 m-0'>
+                    <Sidebar />
+                </Col>
+                <Col md={10} className='p-0 m-0 _outlet'>
+                    <Navbar/>
+                    <Outlet/>
+                </Col>
+            </Row>
+        </div>
+    )
+}
